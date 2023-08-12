6f8f2bf241baa20b242bbbbea6cdd4f980a2bc1b
Max Lee <m.m.lee@student.saintkentigern.com>
2023-08-13 11:02:02 +1200

    agenda iteraton 2 prototpe

--- Web Design/script.js
+++ Web Design/script.js
@@ -1,8 +1,19 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+const smoother = ScrollSmoother.create({
+  content: ".content",
+  smooth: 3,
+  effects: true
+});
+
+smoother.effects("img", { speed: "auto" });
